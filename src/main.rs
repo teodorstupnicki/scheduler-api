c789739e390cc4a7376544df6d8d0047a2994130
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-09-11 20:29:48 +0200

    Updated thread pooling

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use scheduler_api::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+    println!("Shutting down!");
 }
 
 fn handle_connection(mut stream: TcpStream) {
