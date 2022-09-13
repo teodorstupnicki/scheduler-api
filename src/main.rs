f96cfab75e2a4b4c00c36d4e599bc354e4fafda7
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-09-13 23:20:21 +0200

    Added actix-web server

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
-use std::fs;
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::net::TcpListener;
 use scheduler_api::tcp::thread_pool::ThreadPool;
+use actix_web::{App, web, HttpServer};
+use scheduler_api::web::api::*;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
-    println!("Shutting down!");
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "./src/tcp/html/index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "./src/tcp/html/404.html")
-    };
-    let contents = fs::read_to_string(filename).unwrap();
-    let response = format!("{}{}", status_line, contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 5000))?
+    .run()
+    .await
 }
--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1,3 +1,39 @@
 pub mod thread_pool;
 pub mod worker;
 pub mod fn_box;
+
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use thread_pool::ThreadPool;
+
+fn run_server() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+    println!("Shutting down!");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "./src/tcp/html/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "./src/tcp/html/404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
